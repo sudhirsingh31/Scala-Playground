54ed637854793f5aa69a27af5de5e31c0ac1140d
Sudhir Singh <30720095+sudhirsingh31@users.noreply.github.com>
2023-12-01 22:39:01 +0530

    Two Pointer algorithm in Scala

---
+++ twopointer/TwoPointer.scala
@@ -0,0 +1,18 @@
+package twopointer
+object TwoPointer {
+
+  /** Method returns absolute value of the number
+    *
+    * @param Int
+    * @return
+    */
+  def removeElement(nums: Array[Int], `val`: Int): Int = {
+        val res = nums.filter(_!=`val`)
+        res.indices.foldLeft(nums){(nums,i) =>
+        nums(i) = res(i)
+        nums
+        }
+        res.length
+    }
+
+}
